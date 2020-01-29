a97b65c1e96bb3cd29fd70735a0d7596e931e1ec
KyoheiYamamoto <arienkyohei@gmail.com>
2020-01-29 13:51:09 +0000

    test

--- database/migrations/2020_01_07_120303_create_portfolios_table.php
+++ database/migrations/2020_01_07_120303_create_portfolios_table.php
@@ -16,11 +16,11 @@ class CreatePortfoliosTable extends Migration
             $table->string('title'); // ニュースのタイトルを保存するカラム
             $table->string('body');  // ニュースの本文を保存するカラム
             $table->string('image_path')->nullable();  // 画像のパスを保存するカラム
-            $table->timestamps('added_on');
-            $table->string('zip', 10)->comment->nullable()('郵便番号');
-		    $table->tinyInteger('prefecture_code')->unsigned()->comment->nullable('都道府県コード : JIS都道府県コード');
-		    $table->string('prefecture', 40)->comment->nullable('都道府県名 フリーテキスト検索で使用');
-	     	$table->string('address')->comment->nullable('住所2');
+            $table->timestamps();
+            $table->string('zip', 10)->nullable()->comment('郵便番号');
+		    $table->tinyInteger('prefecture_code')->unsigned()->nullable()->comment('都道府県コード : JIS都道府県コード');
+		    $table->string('prefecture', 40)->nullable()->comment('都道府県名 フリーテキスト検索で使用');
+	     	$table->string('address')->nullable()->comment('住所2');
 	     	$table->string('tel', 20)->nullable()->comment('電話番号');
         });
     }
