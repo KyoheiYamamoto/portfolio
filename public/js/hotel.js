00a94fe4c534e75c79dacdf2a38fa2e2e12d2dc9
KyoheiYamamoto <arienkyohei@gmail.com>
2020-02-18 14:24:06 +0000

    test

--- public/js/hotel.js
+++ public/js/hotel.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
     $('.slider').slick({
           dots: true,
+          // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
+          autoplay: true,
+          
+          // 自動再生で切り替えをする時間
+          autoplaySpeed: 2000,
+          // 自動再生や左右の矢印でスライドするスピード
+          speed: 1000,
+          // 自動再生時にスライドのエリアにマウスオンで一時停止するかどうか
+          pauseOnHover: true,
+          // 自動再生時にドットにマウスオンで一時停止するかどうか
+          pauseOnDotsHover: true,
+ 
     });
+    $(function() {
+    $('.autoplay').slick({
+      autoplay: true,
+    }); 
+  });
+    //ここに何か入れる２・１６
 });
+
