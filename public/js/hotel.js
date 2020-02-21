006ac16acc29dd5ef63cb989846ac62f0452c16c
KyoheiYamamoto <arienkyohei@gmail.com>
2020-02-21 23:01:20 +0900

    test

--- public/js/hotel.js
+++ public/js/hotel.js
@@ -15,16 +15,13 @@ $(document).ready(function () {
       $(function() {
       $('.autoplay').slick({
         autoplay: true,
-        
       }); 
         var particles = Particles.init({
       selector: '.background',
     sizeVariations: 10,
     color: ['#00bbdd', '#404B69', '#DBEDF3'],
     connectParticles: true
-});
+ });
     });
- 
-       
-});
 
+});
