081de9103886c70b6f436bc245a0c7b37afec106
KyoheiYamamoto <arienkyohei@gmail.com>
2020-02-22 21:11:04 +0900

    test

--- resources/views/admin/portfolio/edit.blade.php
+++ resources/views/admin/portfolio/edit.blade.php
@@ -58,33 +58,33 @@
                         <label for="star1">★</label>
                       </div>
                     </form>
-                    <div class="form-group row">
-                        <label class="col-md-2" for="image">画像</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                            <div class="form-text text-info">
-                                設定中: {{ $portfolios_form->image_path }}
-                            </div>
-                            <div class="form-check">
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
-                                </label>
+                        <div class="form-group row">
+                            <label class="col-md-2" for="image">画像</label>
+                            <div class="col-md-10">
+                                <input type="file" class="form-control-file" name="image">
+                                <div class="form-text text-info">
+                                    設定中: {{ $portfolios_form->image_path }}
+                                </div>
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                                  <label class="col-md-2" for="title">住所</label>
-                                  <div class="col-md-10">
-                                    <textarea name="example" cols="50" rows="3"></textarea>
-                                  </div>
-                              </div>
-                              <div class="form-group row">
-                                <label class="col-md-2" for="title">電話番号</label>
-                                <div class="col-md-10">
-                                  <textarea name="example" cols="50" rows="10"></textarea>
-                               </div>
-                            </div>
-            
+                        <div class="form-group row">
+                                    <label class="col-md-2" for="title">住所</label>
+                                    <div class="col-md-10">
+                                        <textarea name="example" cols="50" rows="3"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-2" for="title">電話番号</label>
+                                    <div class="col-md-10">
+                                    <textarea name="example" cols="50" rows="10"></textarea>
+                                </div>
+                                </div>
+
                     <div class="form-group row">
                         <div class="col-md-2">
                             <input type="hidden" name="id" value="{{ $portfolios_form->id }}">
@@ -93,7 +93,7 @@
                         </div>
                     </div>
                 </form>
-                
+
                 <div class="row mt-5">
                     <div class="col-md-4 mx-auto">
                         <h2>編集履歴</h2>
@@ -101,7 +101,7 @@
                             @if ($portfolios_form->histories != NULL)
                                 @foreach ($portfolios_form->histories as $history)
                                     <li class="list-group-item">{{ $history->edited_at }}</li>
-                                
+
                                 @endforeach
                             @endif
                         </ul>
