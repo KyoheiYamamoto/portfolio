0dfc7fa2abc22308330d0c7d82c00b2cd58298b2
KyoheiYamamoto <arienkyohei@gmail.com>
2020-03-14 21:04:59 +0900

    0313

--- resources/views/admin/portfolio/create.blade.php
+++ resources/views/admin/portfolio/create.blade.php
@@ -87,7 +87,7 @@
                                         //crop option
                                         // id='imgに適用'
                                         $('#img').cropper({
-                                        aspectRatio: 4 / 4 // ここでアスペクト比の調整 ワイド画面にしたい場合は 16 / 9
+                                        aspectRatio: 3 / 4 // ここでアスペクト比の調整 ワイド画面にしたい場合は 16 / 9
 
                                         });
                                     </script>
@@ -115,7 +115,7 @@
                                         //crop options
                                         // id='imgに適用'
                                         $('#img').cropper({
-                                        aspectRatio: 4 / 4 // ここでアスペクト比の調整 ワイド画面にしたい場合は 16 / 9
+                                        aspectRatio: 3 / 4 // ここでアスペクト比の調整 ワイド画面にしたい場合は 16 / 9
 
                                         });
                                     </script>
@@ -142,12 +142,12 @@
                                         //crop options
                                         // id='imgに適用'
                                         $('#img').cropper({
-                                        aspectRatio: 4 / 4 // ここでアスペクト比の調整 ワイド画面にしたい場合は 16 / 9
+                                        aspectRatio: 3 / 4 // ここでアスペクト比の調整 ワイド画面にしたい場合は 16 / 9
 
                                         });
                                     </script>
 
-                        </div>
+                                 </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-3" for="title">住所</label>
